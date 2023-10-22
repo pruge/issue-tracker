087bec63650eb2fea27d5196b2b6d7e8030fb89d
james kim <strive1024@gmail.com>
2023-10-23 01:24:46 +0900

    Fix: Support sub url pattern in the Navbar

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import Link from 'next/link'
+import {usePathname} from 'next/navigation'
+import React from 'react'
+import {AiFillBug} from 'react-icons/ai'
+import clsx from 'clsx'
+
+const NavBar = () => {
+  const links = [
+    {
+      label: 'Dashboard',
+      href: '/',
+    },
+    {label: 'Issues', href: '/issues'},
+  ]
+
+  const currentPath = usePathname()
+  const createUrlPattern = (href: string) => {
+    if (href === '/') return /^\/$/i
+    return new RegExp(`^\\${href}`, 'i')
+  }
+
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => {
+          const urlPattern = createUrlPattern(link.href)
+          return (
+            <Link
+              key={link.href}
+              className={clsx(
+                {
+                  'text-zinc-900': urlPattern.test(currentPath),
+                  'text-zinc-500': !urlPattern.test(currentPath),
+                },
+                'hover:text-zinc-800 transition-colors',
+              )}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          )
+        })}
+      </ul>
+    </nav>
+  )
+}
+
+export default NavBar
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import './globals.css'
 import type {Metadata} from 'next'
 import {Inter} from 'next/font/google'
 import {Theme, ThemePanel} from '@radix-ui/themes'
-import NavBar from './NavBar'
+import NavBar from './components/NavBar'
 
 const inter = Inter({subsets: ['latin'], variable: '--font-inter'})
 
